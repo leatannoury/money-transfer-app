cc539702d417ea4a3d6083f6c69c0891215fd362
elio1235 <elio123.a9@gmail.com>
2025-11-04 18:09:41 +0200

    make the appserviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Artisan;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         if (Role::count() === 0) {
+        Artisan::call('db:seed', ['--class' => 'RoleSeeder']);
+    }
     }
 }
